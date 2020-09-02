08e4b8cfe7fe1330b76a5f4e9ba319edcf084d2e
mhumam <muhamadhumamm17@gmail.com>
2020-09-02 21:41:37 +0700

    update main layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
+/**
+ * @author Muhamad Humam
+ * @email muhamadhumamm17@gmail.com
+ * @create date 2020-09-02 21:33:08
+ * @modify date 2020-09-02 21:33:08
+ * @desc Main Layout
+ */
+
 import React from 'react';
-import logo from './logo.svg';
+import MainLayout from './pages/home/Index';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Layout extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<MainLayout />
+			</React.Fragment>
+		)
+	}
 }
 
-export default App;
+export default Layout;
