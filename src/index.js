e198ef6ccd5b573b049a0bd2082770fa6f0793f8
mhumam <muhamadhumamm17@gmail.com>
2020-09-02 21:41:19 +0700

    connect redux on root index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+/**
+ * @author Muhamad Humam
+ * @email muhamadhumamm17@gmail.com
+ * @create date 2020-09-02 21:32:47
+ * @modify date 2020-09-02 21:32:47
+ * @desc Root File
+ */
+
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
+import configureStore from './redux/ConfigureStore';
+
+const store = configureStore();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
