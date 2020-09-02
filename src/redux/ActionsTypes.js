5e068207d7cd07b75dccc0388502591efe6328b3
mhumam <muhamadhumamm17@gmail.com>
2020-09-02 21:40:23 +0700

    initalize actions type

---
+++ src/redux/ActionsTypes.js
@@ -0,0 +1,15 @@
+/**
+ * @author Muhamad Humam
+ * @email muhamadhumamm17@gmail.com
+ * @create date 2020-09-02 21:33:42
+ * @modify date 2020-09-02 21:33:42
+ * @desc Action Type For REducer Redux
+ */
+
+export const SELECT_USER = 'SELECT_USER';
+export const REQUEST_USERDATA = 'REQUEST_USERDATA';
+export const RECEIVE_USERDATA = 'RECEIVE_USERDATA';
+export const RECEIVE_USERDATA_ERROR = 'RECEIVE_USERDATA_ERROR';
+export const REQUEST_REPOS = 'REQUEST_REPOS';
+export const RECEIVE_REPOS = 'RECEIVE_REPOS';
+export const RECEIVE_REPOS_ERROR = 'RECEIVE_REPOS_ERROR';
